c87230b4a0e484ecae6a54e01638d6648c521bd7
Smith, Adam <ADAM_SMITH3@comcast.com>
2025-09-18 11:49:43 -0400

    deploy lambda

--- lambda/main.tf
+++ lambda/main.tf
@@ -19,7 +19,7 @@ terraform {
 
 resource "null_resource" "install_python_dependencies" {
   provisioner "local-exec" {
-    command = "bash ${path.module}/lambda/create_pkg.sh"
+    command = "bash ${path.module}/create_pkg.sh"
 
     environment = {
       source_code_path = "${path.module}/lambda"
