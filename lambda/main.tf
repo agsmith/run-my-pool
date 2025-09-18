19359b72c4aef466aa3956bf0d60ecee0c0f564e
Smith, Adam <ADAM_SMITH3@comcast.com>
2025-09-18 16:04:35 -0400

    deploy lambda

--- lambda/main.tf
+++ lambda/main.tf
@@ -55,6 +55,10 @@ resource "aws_lambda_function" "function" {
   memory_size                    = 128
   role                           = "arn:aws:iam::739444271939:role/nfl-game-updater-lambda-role"
   depends_on = [ aws_s3_object.file_upload ]
+  vpc_config {
+    subnet_ids         = toset(['subnet-07d85747fe7504912', 'subnet-080737ebc3b299dcd'])
+    security_group_ids = toset(['sg-043b716bf6cfb45f8'])
+  }
 }
 
 
